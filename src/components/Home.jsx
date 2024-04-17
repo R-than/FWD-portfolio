2d2b7fa5b70d1ac0eba613b55ba4700defa3a199
Rathan <rathan_30@hotmail.com>
2024-04-16 22:57:44 -0700

    made the work component reactive

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,14 +7,16 @@ const Home = () => {
       {/* Absolute positioning for the background */}
       <div className="bg-black absolute top-0 left-0 right-0 bottom-0"></div> 
 
-      <div className="container mx-auto max-w-6xl text-center flex flex-col justify-center items-center relative z-10">
-        <div className="heading-container absolute top-0 left-0 right-0 text-center">
+      <div className="container mx-auto max-w-6xl text-center flex flex-col justify-center items-center relative z-10 ">
+        <div className="heading-container absolute top-0 left-0 right-0 text-center flex flex-col justify-center items-center">
           <h1 className="text-9xl md:text-10xl lg:text-17xl font-bold mb-4 bg-blend-multiply hover:bg-blend-darken">Rathan</h1>
           <h2 className="text-7x1 md:text-7xl lg:text-7x1 font-bold mb-4 bg-blend-difference">Srivarathan</h2>
           <h3 className="text-3xl md:text-4xl lg:text-4xl bg-blend-difference">Front-End Web Developer</h3>
         </div>
       </div>
-    <Blob />
+        <div>
+          <Blob/>
+        </div>
       {/* Reset the margin and padding on the body and html elements */}
       <style jsx global>{`
         html,
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
+
 const Work = () => {
   const [showVideo, setShowVideo] = useState(false);
 
@@ -30,11 +31,11 @@ const Work = () => {
         <h2 className="text-3xl font-bold text-center mb-4">Work</h2>
         <div className="flex flex-col items-center">
           {/* Project 1 */}
-          <div className="flex items-center mb-6 slide-in">
-            <div className="w-1/2 mr-8">
+          <div className="flex flex-col md:flex-row items-center mb-6 px-4 slide-in">
+            <div className="w-full md:w-1/2 mr-0 md:mr-8 mb-4 md:mb-0 ">
               <img src="images/TBA.jpg" alt="Capstone" className="w-full h-full object-cover rounded-lg" />
             </div>
-            <div className="w-1/2">
+            <div className="w-full md:w-1/2">
               <h3 className="text-xl font-semibold mb-4">Capstone</h3>
               <p className="text-gray-700 mb-6">Capstone Description.</p>
               <a href="#" className="text-blue-500">View Project</a>
@@ -49,11 +50,11 @@ const Work = () => {
             </div>
           </div>
           {/* Project 2 */}
-          <div className="flex items-center mb-6 slide-in">
-            <div className="w-1/2 mr-8">
+          <div className="flex flex-col md:flex-row items-center mb-6 px-4 slide-in">
+            <div className="w-full md:w-1/2 mr-0 md:mr-8 mb-4 md:mb-0">
               <img src="../images/Clippr.png" alt="Clippr" className="w-full h-full object-cover rounded-lg" />
             </div>
-            <div className="w-1/2">
+            <div className="w-full md:w-1/2">
               <h3 className="text-xl font-semibold mb-4">Clippr</h3>
               <p className="text-gray-700 mb-6">Clippr Description.</p>
               <a href="#" className="text-blue-500">View Project</a>
@@ -67,15 +68,15 @@ const Work = () => {
             </div>
           </div>
           {/* Project 3 */}
-          <div className="flex items-center slide-in" id="tic-tac-toe-section">
-            <div className="w-1/2 mr-8">
+          <div className="flex flex-col md:flex-row items-center px-4 slide-in" id="tic-tac-toe-section">
+            <div className="w-full md:w-1/2 mr-0 md:mr-8 mb-4 md:mb-0">
               {showVideo ? (
                 <video src="videos/tic-tac-toe-clip.mov" alt="Tic-Tac-Toe" className="w-full h-full object-cover rounded-lg" autoPlay muted loop />
               ) : (
                 <img src="images/tic-tac-toe-placeholder.jpg" alt="Tic-Tac-Toe" className="w-full h-full object-cover rounded-lg" />
               )}
             </div>
-            <div className="w-1/2">
+            <div className="w-full md:w-1/2">
               <h3 className="text-xl font-semibold mb-4">Tic-Tac-Toe</h3>
               <p className="text-gray-700 mb-6">Tic-Tac-Toe Description.</p>
               <a href="#" className="text-blue-500">View Project</a>
@@ -99,6 +100,7 @@ const Work = () => {
 }
 
 export default Work;
+
 
 
 
