45752d473f2503e0188f86cbb7a0cbd4bbb0aaf5
Rathan <rathan_30@hotmail.com>
2024-04-22 03:57:00 -0700

    updated effects in Blob Component

--- src/components/Blob.jsx
+++ src/components/Blob.jsx
@@ -8,11 +8,15 @@ function Blob() {
        const bigBlob = svgRef.current.querySelector('.bigBlob');
        const smallBlob1 = svgRef.current.querySelector('.smallBlob1');
        const smallBlob2 = svgRef.current.querySelector('.smallBlob2');
+       const smallBlob3 = svgRef.current.querySelector('.smallBlob3');
+       const smallBlob4 = svgRef.current.querySelector('.smallBlob4');
    
        if (bigBlob && smallBlob1 && smallBlob2) {
          bigBlob.style.animation = 'orbit 20s infinite linear';
-         smallBlob1.style.animation = 'orbit 15s infinite linear';
-         smallBlob2.style.animation = 'orbit 10s infinite linear reverse';
+         smallBlob1.style.animation = 'orbit 10s infinite linear';
+         smallBlob2.style.animation = 'orbit 20s infinite linear reverse';
+         smallBlob3.style.animation = 'orbit 30s infinite linear reverse';
+         smallBlob4.style.animation = 'orbit 30s infinite linear reverse';
        }
     }, []);
    
@@ -26,14 +30,17 @@ function Blob() {
              </filter>
            </defs>
            <g style={{ filter: 'url(#gooey)' }}>
-             {/* Big bob 1 */}
-             <circle className="bigBlob " cx="55%" cy="60%" r="150" fill="magenta" stroke="black " />
-             {/* Big bob 2 */}
-             <circle className="smallBlob2 " cx="50%" cy="75%" r="80" fill="bisque" stroke="black " />
-             {/* Small bob 1 */}
-             <circle className="smallBlob1 blur-2x1" cx="40%" cy="65%" r="50" fill="cyan" stroke="black" />
-             {/* Small bob 2 */}
-             <circle className="smallBlob2 blur-2x1" cx="50%" cy="45%" r="40" fill="Orange" stroke="black" />
+             {/* Big blob 1 Puprle*/}
+             <circle className="bigBlob " cx="55%" cy="50%" r="150" fill="magenta" stroke="black " />
+             {/* Big blob 1 Yellow*/}
+             <circle className="smallBlob1 " cx="60%" cy="65%" r="50" fill="#FFFF00" stroke="black " />
+             {/* Big blob 2 Green*/}
+             <circle className="smallBlob2 " cx="40%" cy="35%" r="80" fill="#33FF00" stroke="black " />
+             {/* Small blob 3 Cyan*/}
+             <circle className="smallBlob3 blur-2x1" cx="40%" cy="65%" r="50" fill="cyan" stroke="black" />
+             {/* Small blob 4 Red */}
+             <circle className="smallBlob4  blur-2x1" cx="55%" cy="55%" r="30" fill="#FF3300" stroke="black" />
+             
            </g>
          </svg>
        </div>
