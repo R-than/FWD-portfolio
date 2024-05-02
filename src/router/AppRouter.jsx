6f56681a83440dfa4415507dbe1ffafb75f121d6
Rathan <rathan_30@hotmail.com>
2024-05-02 16:17:25 -0700

    added pages

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 //Pages
 import HomePage from "../pages/HomePage";
 import FrothPage from '../pages/FrothPage';
+import ClipprPage from '../pages/ClipprPage';
+import TicTacToePage from '../pages/TicTacToePage';
 
 export default function AppRouter() {
     return (
@@ -11,6 +13,8 @@ export default function AppRouter() {
                 <Routes>
                     <Route path="/" element={<HomePage/>}> </Route>
                     <Route path="/Froth" element={<FrothPage/>}> </Route>
+                    <Route path="/Clippr" element={<ClipprPage/>}> </Route>
+                    <Route path="/TicTacToe" element={<TicTacToePage/>}> </Route>
                 </Routes>
             </BrowserRouter>
         </div>
